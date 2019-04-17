05fb6a50ac348970f07c41b1e40781e5c7aa7c8f
Daryl Sconyers <dsconyer@progressive.com>
2019-04-17 19:46:45 +0000

    Updated Developer responses

--- src/developers/developers.controller.ts
+++ src/developers/developers.controller.ts
@@ -1,4 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
+import * as cuid from 'cuid';
+import { Response } from 'express';
+import { DeveloperRequest } from './DeveloperRequest';
+import { DeveloperResponse } from './DeveloperResponse';
 
 @Controller('developers')
 export class DevelopersController {
@@ -17,6 +21,23 @@ export class DevelopersController {
             }, 0);
         });
     }
+
+    @Post()
+    async addDevelopoer(@Body() dev: DeveloperRequest, @Res() res: Response) {
+        if (dev.firstName === 'darth') {
+            res.status(HttpStatus.BAD_REQUEST).send();
+        } else {
+            const newId = cuid();
+            const response = new DeveloperResponse();
+            response.id = newId;
+            response.firstName = dev.firstName;
+            response.lastName = dev.lastName;
+            response.team = dev.team;
+            this.database.push(response);
+            return res.status(HttpStatus.CREATED).send(response);
+        }
+    }
+
 }
 
 interface Developer {
